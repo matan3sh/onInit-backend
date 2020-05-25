78d3197b1aa0ef5cf6ec55b09a678e0b09a7aa71
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-05-25 18:46:02 +0300

    Implement session authentication, create Auth-API

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -6,15 +6,27 @@ query = async () => {
     return Promise.resolve(users);
   } catch (err) {
     console.log('Error cannot find courses');
+    throw err;
   }
 };
 
 getById = async (id) => {
   try {
-    const user = users.find((user) => user._id == id);
+    const user = users.find((user) => user._id === id);
     return Promise.resolve(user);
   } catch (err) {
     console.log(`Cannot find user with id of ${id}`);
+    throw err;
+  }
+};
+
+getByEmail = async (email) => {
+  try {
+    const user = users.find((user) => user.email === email);
+    return user;
+  } catch (err) {
+    console.log(`ERROR: while finding user ${email}`);
+    throw err;
   }
 };
 
@@ -25,6 +37,7 @@ remove = async (id) => {
     return _saveToFile().then(() => users);
   } catch (err) {
     console.log(`Cannot remove user with id of ${id}`);
+    throw err;
   }
 };
 
@@ -35,6 +48,7 @@ update = async (user) => {
     return _saveToFile().then(() => user);
   } catch (err) {
     console.log(`Error while updating user with id of ${user._id}`);
+    throw err;
   }
 };
 
@@ -42,10 +56,14 @@ add = async (user) => {
   try {
     user._id = _makeId();
     user.createdAt = Date.now();
+    user.avatar =
+      'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png';
+    user.isAdmin = false;
     users.unshift(user);
     return _saveToFile().then(() => user);
   } catch (err) {
     console.log('Error while adding user');
+    throw err;
   }
 };
 
@@ -55,6 +73,7 @@ module.exports = {
   remove,
   add,
   update,
+  getByEmail,
 };
 
 function _saveToFile() {
